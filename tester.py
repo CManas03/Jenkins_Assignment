0ac8118bbd235687b5428db850957e1e284044a9
CManas03 <Chaitanya.Manas@iiitb.ac.in>
2022-11-24 19:26:48 +0530

    All Required Files

---
+++ tester.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import unittest
+from Task1 import Add
+from Task2 import Mul
+
+class Calculator(unittest.TestCase):
+
+    def test_add(self):
+        x = 10
+        y = 20
+        sum = Add(x, y)
+        self.assertEqual(sum, 50)
+
+    def test_mult(self):
+        x = 10
+        y = 20
+        mult = Mul(x, y)
+        self.assertEqual(mult, 50)
+
+if __name__ == '__main__':
+    unittest.main()
